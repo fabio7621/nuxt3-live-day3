cf46bd0812e57e2b48ec8f956e36799b559ad8ef
fabio7621 <d6200121a@gmail.com>
2024-11-16 10:28:01 +0800

    first commit

---
+++ layouts/admin.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div>
+    <slot />
+  </div>
+  <h1>現在是ADMIN layout</h1>
+</template>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div>
+    <slot />
+  </div>
+  <h1>現在是預設layout</h1>
+</template>
+
+<style scoped></style>
